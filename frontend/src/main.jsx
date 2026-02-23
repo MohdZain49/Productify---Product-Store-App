29b43e71671cb514c90d892306e7a6244a2411b5
Mohd Zain <mohammedzain4099@gmail.com>
2026-02-23 13:20:40 +0530

    create frontend setup

---
+++ frontend/src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+import App from "./App.jsx";
+
+import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter } from "react-router";
+
+// Import Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Clerk Publishable Key not found");
+}
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
+  </StrictMode>,
+);
