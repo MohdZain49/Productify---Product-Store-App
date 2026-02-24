6e5e251f4cbdd076719012d048882e120a7afc6a
Mohd Zain <mohammedzain4099@gmail.com>
2026-02-24 11:32:28 +0530

    feat: implement Navbar component with authentication

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import { Link } from "react-router";
+import {
+  SignInButton,
+  SignUpButton,
+  UserButton,
+  useAuth,
+} from "@clerk/clerk-react";
+import { ShoppingBagIcon, PlusIcon, UserIcon } from "lucide-react";
+import ThemeSelector from "./ThemeSelector";
 
 function Navbar() {
+  const { isSignedIn } = useAuth();
+
   return (
-    <nav>navbar</nav>
-  )
+    <nav className="navbar bg-base-100">
+      <div className="max-w-5xl w-full mx-auto px-4 flex justify-between items-center ">
+        {/* LOGO - LEFT SIDE */}
+        <div className="flex-1">
+          <Link className="btn btn-ghost gap-2">
+            <ShoppingBagIcon className="size-5 text-primary" />
+            <span className="text-lg font-bold font-mono uppercase tracking-wider">
+              Productify
+            </span>
+          </Link>
+        </div>
+
+        <div className="flex gap-2 items-center">
+          <ThemeSelector />
+          {isSignedIn ? (
+            <>
+              <Link to={"/create"} className="btn btn-primary btn-sm gap-1">
+                <PlusIcon className="size-4" />
+                <span className="hidden sm:inline">New Product</span>
+              </Link>
+              <Link to={"/profile"} className="btn btn-ghost btn-sm gap-1">
+                <UserIcon className="size-4" />
+                <span className="hidden sm:inline">Profile</span>
+              </Link>
+              <UserButton />
+            </>
+          ) : (
+            <>
+              <SignInButton mode="modal">
+                <button className="btn btn-ghost btn-sm">Sign In</button>
+              </SignInButton>
+              <SignUpButton mode="modal">
+                <button className="btn btn-primary btn-sm">Get Started</button>
+              </SignUpButton>
+            </>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
