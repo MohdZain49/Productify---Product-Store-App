d97b25213225b760e78d1b0a6599e3ac323703fb
Mohd Zain <mohammedzain4099@gmail.com>
2026-03-01 12:14:51 +0530

    feat: integrate axios and react-query for API management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,8 @@ import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 // Import Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -14,11 +16,16 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Clerk Publishable Key not found");
 }
 
+// Create a client
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <BrowserRouter>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </BrowserRouter>
     </ClerkProvider>
   </StrictMode>,
