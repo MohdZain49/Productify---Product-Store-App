f07bdc155ffd61152f65b177dd9a666928e87a4b
Mohd Zain <mohammedzain4099@gmail.com>
2026-03-04 14:09:48 +0530

    feat: enhance routing with authentication checks and update product hooks for creation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 
 import Navbar from "./components/Navbar";
 
@@ -12,11 +12,10 @@ import useAuthReq from "./hooks/useAuthReq";
 import useUserSync from "./hooks/useUserSync";
 
 function App() {
-  const { isClerkLoaded } = useAuthReq();
+  const { isClerkLoaded, isSignedIn } = useAuthReq();
   useUserSync();
 
   if (!isClerkLoaded) return null;
-
 
   return (
     <div className="min-h-s}creen bg-base-100">
@@ -25,9 +24,18 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/product/:id" element={<ProductPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/create" element={<CreateProductPage />} />
-          <Route path="/edit/:id" element={<EditProductPage />} />
+          <Route
+            path="/profile"
+            element={isSignedIn ? <ProfilePage /> : <Navigate to={"/"} />}
+          />
+          <Route
+            path="/create"
+            element={isSignedIn ? <CreateProductPage /> : <Navigate to={"/"} />}
+          />
+          <Route
+            path="/edit/:id"
+            element={isSignedIn ? <EditProductPage /> : <Navigate to={"/"} />}
+          />
         </Routes>
       </main>
     </div>
