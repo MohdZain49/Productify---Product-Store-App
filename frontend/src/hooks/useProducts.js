8b60978e64fab0040e51cf13e46b0206db9e628c
Mohd Zain <mohammedzain4099@gmail.com>
2026-03-05 10:05:00 +0530

    feat: add product retrieval and deletion hooks to useProducts

--- frontend/src/hooks/useProducts.js
+++ frontend/src/hooks/useProducts.js
@@ -1,5 +1,10 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { createProduct, getAllProducts } from "../lib/api";
+import {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  deleteProduct,
+} from "../lib/api";
 
 export const useProducts = () => {
   const result = useQuery({
@@ -13,3 +18,20 @@ export const useCreateProduct = () => {
   const result = useMutation({ mutationFn: createProduct });
   return result;
 };
+
+export const useProduct = (id) => {
+  const result = useQuery({
+    queryKey: ["product", id],
+    queryFn: () => getProductById(id),
+    enabled: !!id, // double bang operator - convert the given value in the boolean value
+  });
+  return result;
+};
+
+export const useDeleteProduct = () => {
+  const result = useMutation({
+    mutationFn: (id) => deleteProduct(id),
+  });
+  return result;
+};
+
